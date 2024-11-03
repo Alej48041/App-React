d7f5ae31610cf3323ddfb8daae3b5b4a653f2f03
Alejandro Rodriguez <89889482+Alej48041@users.noreply.github.com>
2024-11-03 16:05:48 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@
 import React, { useState } from 'react';
 import FormView from './components/FormView';
 import PackageList from './components/PackageList';
-import 'antd/dist/reset.css'; // Importar los estilos de Ant Design
+import 'antd/dist/reset.css'; 
 
 function App() {
-  const [currentView, setCurrentView] = useState('formView'); // Estado para controlar qué vista se muestra
-  const [formData, setFormData] = useState(null); // Estado para almacenar los datos del formulario
+  const [currentView, setCurrentView] = useState('formView'); 
+  const [formData, setFormData] = useState(null); 
 
-  // Esta función se llama cuando se hace clic en el botón "Siguiente" en FormView
   const handleFormSubmit = (data) => {
     setFormData(data);
-    setCurrentView('packageList'); // Cambiar a la vista de paquetes
+    setCurrentView('packageList');
   };
 
-  // Función para manejar el botón "Regresar" desde la vista de PackageList
   const handleBackToForm = () => {
-    setCurrentView('formView'); // Cambiar a la vista del formulario
+    setCurrentView('formView');
   };
 
   return (
